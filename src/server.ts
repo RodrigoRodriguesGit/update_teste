5b365d08c6f4bf8854aec15f3e0b39cd0b43f6c2
Rodrigo Rodrigues <rodrigo@go2sistemas.local>
2023-05-26 16:24:00 -0300

    Teste de timeout!

--- src/server.ts
+++ src/server.ts
@@ -59,10 +59,10 @@ app.post('/uploadCSV', upload.single('file'), async (request, response) => {
 // });
 
 app.post('/upload', upload.single('file'), async (request, response) => {
+  request.setTimeout(80000);
   const { file } = request;
-  setTimeout(() => {
-    response.send(file);
-  }, 30000);
+  // request.setTimeout(10 * 80 * 100);
+  response.send(file);
 });
 
 app.listen(3000, () => {
