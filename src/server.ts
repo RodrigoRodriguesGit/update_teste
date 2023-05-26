379f8fb622db6f4edd4b689f968fbb241567af8c
Rodrigo Rodrigues <rodrigo@go2sistemas.local>
2023-05-26 10:14:23 -0300

    first commit

---
+++ src/server.ts
@@ -0,0 +1,63 @@
+import express from 'express';
+import multer from 'multer';
+import fs from 'fs';
+import csvParse from 'csv-parser';
+
+// Antigo para teste, primeira forma
+const upload = multer({
+  dest: './tmp',
+});
+
+const app = express();
+
+interface Upload {
+  name: string;
+  description: string;
+}
+
+function loadCategories(file: Express.Multer.File): Promise<Upload[]> {
+  return new Promise((resolver, reject) => {
+    const stream = fs.createReadStream(file.path);
+
+    const parseFile = csvParse();
+
+    stream.pipe(parseFile);
+
+    const categories: Upload[] = [];
+
+    parseFile
+      .on('data', async line => {
+        // eslint-disable-next-line no-console
+        console.log(line);
+        const { name, description } = line;
+        categories.push({ description, name });
+      })
+      .on('end', () => {
+        resolver(categories);
+      })
+      .on('error', err => {
+        reject(err);
+      });
+  });
+}
+
+app.post('/uploadCSV', upload.single('file'), async (request, response) => {
+  const { file } = request;
+  const categories = await loadCategories(file);
+  response.send(categories);
+});
+
+// app.post('/uploadCSV', upload.single('file'), async (request, response) => {
+// const { file } = request;
+// const categories = await loadCategories(file);
+// response.send(categories);
+// });
+
+app.post('/upload', upload.single('file'), async (request, response) => {
+  const { file } = request;
+  response.send(file);
+});
+
+app.listen(3000, () => {
+  console.log('🏃 Running Server');
+});
